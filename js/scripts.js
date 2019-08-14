4fdf52317c67029be1f7bd19a07b3ce098bbe815
Jordan Safford and Victor Guttmann <jordan@>
2019-08-14 10:21:24 -0700

    added subtract,multiply,divide

--- js/scripts.js
+++ js/scripts.js
@@ -19,11 +19,37 @@ var divide = function(number1, number2) {
 // Everything below this line is user interface (or front-end) logic:
 
 $(document).ready(function() {
-	$("form#add").submit(function(event) {
+
+  $("form#add").submit(function(event) {
 event.preventDefault();
 	  var number1 = parseInt($("#add1").val());
 	  var number2 = parseInt($("#add2").val());
 		var result = add(number1,number2);
 		$("#output").text(result);;
 	});
+
+  $("form#subtract").submit(function(event) {
+event.preventDefault();
+	  var number1 = parseInt($("#sub1").val());
+	  var number2 = parseInt($("#sub2").val());
+		var result = subtract(number1,number2);
+		$("#output").text(result);;
+	});
+
+  $("form#multiply").submit(function(event) {
+event.preventDefault();
+    var number1 = parseInt($("#mul1").val());
+    var number2 = parseInt($("#mul2").val());
+    var result = multiply(number1,number2);
+    $("#output").text(result);;
+  });
+
+  $("form#divide").submit(function(event) {
+event.preventDefault();
+    var number1 = parseInt($("#div1").val());
+    var number2 = parseInt($("#div2").val());
+    var result = divide(number1,number2);
+    $("#output").text(result);;
+  });
+
 });
